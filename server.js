dc87bc95c7b3035adffc9af7d5ab9b56220e0d1b
Eak Netpanya <eak.netpanya@gmail.com>
2019-01-23 22:09:25 +0700

    init project

---
+++ server.js
@@ -0,0 +1,8 @@
+const http = require('http');
+const app = require('./app');
+
+const port = process.env.PORT || 3001;
+
+http.createServer(app).listen(port, () => {
+    console.log(`Server is running on localhost:${port}`);
+});
